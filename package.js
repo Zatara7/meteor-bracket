cf38cf28e6c80bcab20dca7cdd6e054c83b230e3
Hayder Sharhan <hayder.net@gmail.com>
2016-02-06 17:02:07 -0500

    Adding package.js file & teijo's module

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'zatara7:jquery-bracket',
+  version: '0.7.3',
+  // Brief, one-line summary of the package.
+  summary: 'jQuery bracket is a jQuery plugin that lets users create and display single and double elimination brackets for tournament play.',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/Zatara7/meteor-bracket.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('0.9.0');
+  api.use('jquery', 'client');
+  api.imply('jquery', 'client');
+  api.addFiles([
+      'jquery-bracket/dist/jquery.bracket.min.js',
+      'jquery-bracket/dist/jquery.bracket.min.css'
+    ], 'client'
+  );
+});
